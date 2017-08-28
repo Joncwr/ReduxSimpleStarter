eacebb79bddce5226443828e483826c1a2903f99
Jonathan Chua <Joncwrrr@gmail.com>
2017-08-29 01:41:55 +0800

    First Lesson Done

---
+++ src/actions/index.js
@@ -0,0 +1,7 @@
+export function selectBook(book) {
+// select book with an actionCreator, it needs to return an action, an object with a type property
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  }
+}
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+  return [
+    { title: 'lol by chua', pages: 101 },
+    { title: 'kekekek by jon', pages: 111101},
+    { title: 'harry potter', pages: 1011},
+    { title: 'science book', pages: 1021},
+    { title: 'secret tomee', pages: 10121}
+  ]
+}
